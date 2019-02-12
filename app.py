b492405cdce642712d973a112cfbf36ad8b02a25
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2019-02-11 19:49:35 -0600

    Broswer Automation with Selenium

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from selenium import webdriver
-
+import config
 browser = webdriver.Chrome()
 browser.get("https://github.com")
+
+signin_link = browser.find_element_by_link_text("Sign in")
+signin_link.click()
+
+username_box = browser.find_element_by_id("login_field")
+username_box.send_keys("keenwilson")
+password_box = browser.find_element_by_id("password")
+password_box.send_keys(config.password)
+password_box.submit()
+
+assert "keenwilson" in browser.page_source
+
+profile_link = browser.find_element_by_class_name("user-profile-link")
+link_label = profile_link.get_attribute("innerHTML")
+
+assert "keenwilson" in link_label
+
+browser.quit()
